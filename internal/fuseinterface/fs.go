941b4890b98bbb61a2e470ff7eacb7834be1855b
Nikolas Sepos <nikolas.sepos@gmail.com>
2024-02-18 18:33:59 +0200

    wip

--- internal/fuseinterface/fs.go
+++ internal/fuseinterface/fs.go
@@ -3,6 +3,7 @@ package fuseinterface
 import (
 	"context"
 	"fmt"
+	"io"
 	iofs "io/fs"
 	"log"
 	"log/slog"
@@ -26,6 +27,7 @@ type ociFS struct {
 	lp        layout.Path
 	fss       []*tarfs.FS
 	fileNames []string
+	fssrcs    []layerdb.ReaderAtCloser
 }
 
 func NewOciFS(imageRef string) (fs.InodeEmbedder, error) {
@@ -73,15 +75,17 @@ func NewOciFS(imageRef string) (fs.InodeEmbedder, error) {
 	}
 
 	ldb := layerdb.NewLayerDB("./layers")
+	var fssrcs []layerdb.ReaderAtCloser
 	var fss []*tarfs.FS
 	var files [][]entry
 
-	for i, layer := range layers {
+	for _, layer := range layers {
 		rc, err := ldb.Get(layer)
 		if err != nil {
 			fmt.Printf("Error getting layer from db: %v", err)
 			return nil, err
 		}
+		fssrcs = append(fssrcs, rc)
 		fs, err := tarfs.New(rc)
 		if err != nil {
 			fmt.Printf("Error creating filesystem: %v", err)
@@ -90,7 +94,6 @@ func NewOciFS(imageRef string) (fs.InodeEmbedder, error) {
 		fss = append(fss, fs)
 		layerFiles := []entry{}
 		if err := iofs.WalkDir(fs, ".", func(path string, d iofs.DirEntry, err error) error {
-			fmt.Printf("path: %v, layer: %v\n", path, i)
 			if err != nil {
 				return err
 			}
@@ -107,6 +110,7 @@ func NewOciFS(imageRef string) (fs.InodeEmbedder, error) {
 	return &ociFS{
 		lp:        lp,
 		fss:       fss,
+		fssrcs:    fssrcs,
 		files:     unf,
 		fileNames: fls,
 	}, nil
@@ -114,11 +118,23 @@ func NewOciFS(imageRef string) (fs.InodeEmbedder, error) {
 
 var _ = (fs.NodeOnAdder)((*ociFS)(nil))
 
+func fileInfoToAttr(fi iofs.FileInfo) *fuse.Attr {
+	return &fuse.Attr{
+		Mode:   uint32(fi.Mode().Perm()),
+		Size:   uint64(fi.Size()),
+		Blocks: uint64(fi.Size() / 512),
+		Atime:  uint64(fi.ModTime().Unix()),
+		Mtime:  uint64(fi.ModTime().Unix()),
+		Ctime:  uint64(fi.ModTime().Unix()),
+	}
+}
+
 func (ofs *ociFS) OnAdd(ctx context.Context) {
 	slog.Info("OnAdd")
 
 	for _, f := range ofs.fileNames {
 		dir, base := filepath.Split(filepath.Clean(f))
+		entry := ofs.files[f]
 
 		p := &ofs.Inode
 		for _, part := range strings.Split(dir, "/") {
@@ -132,8 +148,19 @@ func (ofs *ociFS) OnAdd(ctx context.Context) {
 			}
 			p = ch
 		}
-		entry := ofs.files[f]
-		ch := p.NewPersistentInode(ctx, &ociFile{layer: ofs.fss[entry.layer], entry: entry.entry, path: f}, fs.StableAttr{})
+		finfo, err := entry.entry.Info()
+		if err != nil {
+			fmt.Printf("Error getting file info: %v", err)
+			continue
+		}
+		attr := fileInfoToAttr(finfo)
+		ch := p.NewPersistentInode(ctx, &ociFile{
+			layer: ofs.fss[entry.layer],
+			entry: entry.entry,
+			path:  f,
+			attr:  attr,
+		}, fs.StableAttr{})
+		slog.Info("Added file", "path", f, "layer", entry.layer)
 		p.AddChild(base, ch, true)
 
 	}
@@ -143,6 +170,7 @@ type ociFile struct {
 	fs.Inode
 	entry iofs.DirEntry
 	layer *tarfs.FS
+	attr  *fuse.Attr
 	path  string
 }
 
@@ -152,26 +180,57 @@ var (
 )
 
 func (of *ociFile) Open(ctx context.Context, openFlags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+	slog.Info("Open", "path", of.path)
+
 	f, err := of.layer.Open(of.path)
 	if err != nil {
 		log.Printf("Error opening file: %v", err)
 		return nil, 0, syscall.EIO
 	}
-	return &ociFileHandle{f: f}, 0, fs.OK
+	return &ociFileHandle{f: f, size: of.attr.Size}, fuse.FOPEN_KEEP_CACHE, fs.OK
 }
 
 type ociFileHandle struct {
-	f iofs.File
+	f    iofs.File
+	size uint64
 }
 
 func (gf *ociFile) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	slog.Info("Read", "path", gf.path, "offset", off)
+
 	ofh, ok := fh.(*ociFileHandle)
 	if !ok {
+		slog.Error("Error getting file handle", "path", gf.path, "offset", off)
 		return nil, syscall.EIO
 	}
-	_, err := ofh.f.Read(dest)
-	if err != nil {
+
+	data := make([]byte, ofh.size)
+
+	end := int(off) + len(dest)
+	if end > len(data) {
+		end = len(data)
+	}
+
+	_, err := ofh.f.Read(data)
+	if err != nil && err != io.EOF {
+		slog.Error("Error reading file", "path", gf.path, "offset", off, "error", err)
 		return nil, syscall.EIO
 	}
-	return fuse.ReadResultData(dest), fs.OK
+
+	return fuse.ReadResultData(data[off:end]), fs.OK
 }
+
+var _ = (fs.NodeGetattrer)((*ociFile)(nil))
+
+func (f *ociFile) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Attr = *f.attr
+	return fs.OK
+}
+
+// var _ = (fs.NodeWriter)((*ociFile)(nil))
+//
+// func (f *ociFile) Write(ctx context.Context, fh fs.FileHandle, data []byte, off int64) (written uint32, errno syscall.Errno) {
+// 	slog.Info("Write", "path", f.path, "offset", off, "data", data)
+//
+// 	return 0, syscall.EIO
+// }
