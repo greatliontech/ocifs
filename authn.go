d7cd505efeac3686f11f7d29dc2732a80f8f2629
nikolas sepos <nikolas.sepos@gmail.com>
2024-12-31 10:21:53 +0200

    debug log authn

--- authn.go
+++ authn.go
@@ -1,6 +1,7 @@
 package ocifs
 
 import (
+	"log/slog"
 	"strings"
 
 	"github.com/google/go-containerregistry/pkg/authn"
@@ -13,8 +14,10 @@ type ocifsKeychain struct {
 
 // Resolve looks up the most appropriate credential for the specified target.
 func (o *ocifsKeychain) Resolve(res authn.Resource) (authn.Authenticator, error) {
+	slog.Debug("resolving creds for", "resource", res.String())
 	for k, v := range o.creds {
 		if strings.HasPrefix(k, res.String()) {
+			slog.Debug("found creds for prefix", "prefix", k)
 			return authn.FromConfig(v), nil
 		}
 	}
